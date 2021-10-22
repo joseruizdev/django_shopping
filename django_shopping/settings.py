9c0786bd71943f30189b546dd0f21ee9a5cdfbd8
Jose Ruiz <68619849+joseruizdev@users.noreply.github.com>
2021-10-22 12:56:06 -0500

    Update settings.py

--- django_shopping/settings.py
+++ django_shopping/settings.py
@@ -22,7 +22,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^qykogc58l#en!7t5pw453yduf@t#zrx8hrvul*xecw=pt$9$('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
